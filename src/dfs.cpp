2bddf1d1732d1527da2ca2d4e15f93ed1cee67e5
Avery Nelson <123122545+averymnelson@users.noreply.github.com>
2023-09-22 16:39:08 -0500

    hmm wont load

--- src/dfs.cpp
+++ src/dfs.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 
 /*
-To iterate over the adjancy nodes of u, you can use the following template code 
-int numberOfAdjacencyNodes = G.e[u].size(); // Get number of adjancy nodes of u 
+To iterate over the adjancy nodes of u, you can use the following template code
+int numberOfAdjacencyNodes = G.e[u].size(); // Get number of adjancy nodes of u
 LinkedListNode<int> *p = G.e[u].getRoot(); // Get the head point of the linked list
 for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) { // iterate over each node
     int v = p->value; // v is the adjvancy node of u
@@ -11,24 +11,24 @@ for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) { // iterate ov
 }
 */
 
-int dfs(Graph &G, int start, int destination, int numberOfBuilding, std::vector<int> &path) {
+int dfs(Graph &G, int start, int destination, int numberOfBuilding, std::vector<int> &path)
+{
     int N = G.n; // Number of nodes in the graph
     int p = 0;
     std::string pw = "";
     std::vector<bool> visit(N, false);
     Stack<int> s;
     s.push(start);
-    while(!s.empty()){
+    while (!s.empty())
+    {
         int st = s.pop();
-        if(!visit[st]){
-            std::cout<<st<<" ";
-            p+=1;
-            pw+=st;
-            pw+=" ";
+        if (!visit[st])
+        {
             visit[st] = true;
-        int nAdjnodes = G.e[st].size();
-        LinkedListNode<int> *p = G.e[st].getRoot();
-        for (int i = 0; i<nAdjnodes; i++, p = p->next){
+            int nAdjnodes = G.e[st].size();
+            LinkedListNode<int> *p = G.e[st].getRoot();
+            for (int i = 0; i < nAdjnodes; i++, p = p->next)
+            {
                 int v = p->value;
                 if (!visit[v])
                     s.push(v);
@@ -36,20 +36,19 @@ int dfs(Graph &G, int start, int destination, int numberOfBuilding, std::vector<
         }
     }
 
-    
-//     DFS(G,v) ( v is the vertex where the search starts ) 
-// Stack S := {}; ( start with an empty stack ) 
-// for each vertex u, set visited[u] := false; 
-// push S, v; 
-// while (S is not empty) do 
-// u := pop S; 
-// if (not visited[u]) then 
-//  visited[u] := true; 
-//  for each unvisited neighbor w of u 
-// push S, w; 
-// end if 
-// end while 
-// END DFS()
+    //     DFS(G,v) ( v is the vertex where the search starts )
+    // Stack S := {}; ( start with an empty stack )
+    // for each vertex u, set visited[u] := false;
+    // push S, v;
+    // while (S is not empty) do
+    // u := pop S;
+    // if (not visited[u]) then
+    //  visited[u] := true;
+    //  for each unvisited neighbor w of u
+    // push S, w;
+    // end if
+    // end while
+    // END DFS()
 
-    return 0; //You do not need to count the number of paths in this question, therefore, just simply return 0
+    return 0; // You do not need to count the number of paths in this question, therefore, just simply return 0
 }
