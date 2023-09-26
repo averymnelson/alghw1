7f0323df6579e4cc7bd3e8a91a85e7d5b233ffda
Avery Nelson <123122545+averymnelson@users.noreply.github.com>
2023-09-25 22:45:30 -0500

    adding code based on pseudo from class

--- src/dfs.cpp
+++ src/dfs.cpp
@@ -1,85 +1,41 @@
 #include <search.hpp>
 #include <vector>
 
-/*
-To iterate over the adjancy nodes of u, you can use the following template code
-int numberOfAdjacencyNodes = G.e[u].size(); // Get number of adjancy nodes of u
-LinkedListNode<int> *p = G.e[u].getRoot(); // Get the head point of the linked list
-for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) { // iterate over each node
-    int v = p->value; // v is the adjvancy node of u
-    // YOUR CODE HERE
-}
-*/
-
 int dfs(Graph &G, int start, int destination, int numberOfBuilding, std::vector<int> &path)
 {
-//  int dfs(Graph &G, int start, int destination, int numberOfBuilding, std::vector<int> &path) {
-//  int N = G.n; // Number of nodes in the graph
-//  for u = 0 to N – 1 do
-//  visited[u] = false
-//  trace[u] = -1
-//  end for
-//  Push start to stack
-//  visited[start]= true
-//  while stack is not empty do
-//  u = pop the top value of the stack
-//  if u is destination then
-//  stop while loop
-//  end if
-//  for v in the list of neighbor vertices of u do
-//  if visited[v] is false then
-//  visited[v] = true
-//  trace[v] = u
-//  Push v into stack
-//  end if
-//  end for
-//  end while
-//  u = destination
-//  while u != -1 do
-//  Insert u into the front of path
-//  u = trace[u]
-//  end while
-//  // How to limit the number of building in Recursive DFS and DFS?
-// }
-
     int N = G.n; // Number of nodes in the graph
     int p = 0;
     std::string pw = "";
     std::vector<bool> visit(N, false);
+    std::vector<int> trace(N, -1);
     Stack<int> s;
     s.push(start);
+    visit[start] = true;
     while (!s.empty())
     {
         int st = s.pop();
-        if (!visit[st])
+        if (st == destination)
         {
-            visit[st] = true;
-            int nAdjnodes = G.e[st].size();
-            LinkedListNode<int> *p = G.e[st].getRoot();
-            for (int i = 0; i < nAdjnodes; i++, p = p->next)
+            break;
+        }
+        visit[st] = true;
+        int nAdjnodes = G.e[st].size();
+        LinkedListNode<int> *p = G.e[st].getRoot();
+        for (int i = 0; i < nAdjnodes; i++, p = p->next)
+        {
+            int v = p->value;
+            if (!visit[v])
             {
-                int v = p->value;
-                if (!visit[v])
-                    visit[v]=true;
-                    s.push(v);
-
+                visit[v] = true;
+                s.push(v);
             }
         }
     }
-
-    //     DFS(G,v) ( v is the vertex where the search starts )
-    // Stack S := {}; ( start with an empty stack )
-    // for each vertex u, set visited[u] := false;
-    // push S, v;
-    // while (S is not empty) do
-    // u := pop S;
-    // if (not visited[u]) then
-    //  visited[u] := true;
-    //  for each unvisited neighbor w of u
-    // push S, w;
-    // end if
-    // end while
-    // END DFS()
+    int u = destination;
+    while (u != -1) {
+        path.push_back(u);
+        u = trace[u];
+    }
 
     return 0; // You do not need to count the number of paths in this question, therefore, just simply return 0
 }
--- src/rdfs.cpp
+++ src/rdfs.cpp
@@ -1,61 +1,36 @@
 #include <search.hpp>
-/*
-To iterate over the adjancy nodes of u, you can use the following template code 
-int numberOfAdjacencyNodes = G.e[u].size(); // Get number of adjancy nodes of u 
-LinkedListNode<int> *p = G.e[u].getRoot(); // Get the head point of the linked list
-for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) { // iterate over each node
-    int v = p->value; // v is the adjvancy node of u
-    // YOUR CODE HERE
-}
-*/
 
-int rdfs(Graph &G, int start, int destination, int numberOfBuilding, std::vector<int> &path) {
+int rdfs(Graph &G, int start, int destination, int numberOfBuilding, std::vector<int> &path)
+{
     int N = G.n; // Number of nodes in the graph
-    
-//     DFS(G, u)
-// {
-// u.visited = true
-// for each v ∈ G.Adj[u]
-// if v.visited == false
-// DFS(G,v)
-// }
-// init()
-// {
-// for each u ∈ G
-// u.visited = false
-//  for each u ∈ G
-// DFS(G, u)
-// }
-
-// int dfs(Graph &G, int start, int destination, int numberOfBuilding, std::vector<int> &path) {
-//  int N = G.n; // Number of nodes in the graph
-//  for u = 0 to N – 1 do
-//  visited[u] = false
-//  trace[u] = -1
-//  end for
-//  Push start to stack
-//  visited[start]= true
-//  while stack is not empty do
-//  u = pop the top value of the stack
-//  if u is destination then
-//  stop while loop
-//  end if
-//  for v in the list of neighbor vertices of u do
-//  if visited[v] is false then
-//  visited[v] = true
-//  trace[v] = u
-//  Push v into stack
-//  end if
-//  end for
-//  end while
-//  u = destination
-//  while u != -1 do
-//  Insert u into the front of path
-//  u = trace[u]
-//  end while
-//  // How to limit the number of building in Recursive DFS and DFS?
-// }
-
-    return 0; //You do not need to count the number of paths in this question, therefore, just simply return 0
+    std::vector<bool> visit(N, false);
+    std::vector<int> trace(N, -1);
+    Stack<int> stk;
+    stk.push(start);
+    visit[start] = true;
+    while (!stk.empty())
+    {
+        int u = stk.pop();
+        if (u == destination)
+        {
+            return 1;
+            break;
+        }
+        int numberOfAdjacencyNodes = G.e[u].size();
+        LinkedListNode<int> *p = G.e[u].getRoot();
+        for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next)
+        {
+            int v = p->value;
+            if (!visit[v])
+            {
+                int flag = rdfs(G, v, destination, numberOfBuilding, path);
+                if (flag == 1)
+                {
+                    return 1;
+                }
+            }
+        }
+        path.pop_back();
+    }
+        return 0; // You do not need to count the number of paths in this question, therefore, just simply return 0
 }
-
