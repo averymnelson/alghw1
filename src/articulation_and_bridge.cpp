1b1428d7a7e2027ef392f62c6891de455e067ec9
Avery Nelson <123122545+averymnelson@users.noreply.github.com>
2023-10-02 11:33:51 -0500

    articulation is still wrong but it compiles so

--- src/articulation_and_bridge.cpp
+++ src/articulation_and_bridge.cpp
@@ -1,39 +1,59 @@
 #include <graph.hpp>
 #include <search.hpp>
 
-// To create a pair of node u and v, you can use the following template
-// std::pair<int, int> bride(u, v);
- 
-// void DFS(std::vector<int> &disc, std::vector<int> &low, std::vector<int> &visited, std::vector<int> parent, int vertex){
-//         visited[vertex] = true;
-//         int temp = time + 1;
-//         disc[vertex] = temp;
-//         low[vertex] = temp;
-//         int child = 0;
-//         int nAdjnodes = G.e[vertex].size();
-//         LinkedListNode<int> *p = G.e[vertex].getRoot();
-//         for (int i = 0; i < nAdjnodes; i++, p = p->next)
-//         {
-//             int v = p->value;
-//                         if (!visited[i]){
-//                                 child = child + 1;
-//                                 parent[i] = vertex;
-//                                 DFS(disc, low, visited, parent, i, time+1);
-//                                 low[vertex] = min(low[vertex], low[i]);
-//                                 if (low[i] > disc[vertex]){
-//                                         std::cout<<vertex<<" "<<i<<std::endl;
-//                                         //print vertex, i //bridge
-//                                 }
-//                         }
-//                         else if (parent[vertex] != i){
-//                                 low[vertex] = minimum(low[vertex], disc[i])
-//                         }
-//         }
-// }
-
-void findArticulationPointsAndBridges(Graph &G, std::vector<int> &articulationPoints, std::vector<std::pair<int, int> > &bridges)
+void artdfs(Graph &G, int curr, int d, std::vector<int> &low, std::vector<int> &depth, std::vector<int> &parent, std::vector<int> &articulationPoints, std::vector<std::pair<int, int>> &bridges)
 {
-//     int n = G.n; // Number of Nodes
-//     int time = 0;
-//     DFS(time, time, visited, parent, vertex)
+        low[curr] = d;
+        depth[curr] = d;
+        d++;
+        int children = 0;
+        int numberOfAdjacencyNodes = G.e[curr].size();
+        LinkedListNode<int> *p = G.e[curr].getRoot();
+        for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next)
+        {
+                int v = p->value;
+                if (depth[v] == -1)
+                {
+                        children++;
+                        parent[v] = curr;
+                        artdfs(G, v, d, low, depth, parent, articulationPoints, bridges);
+                        if (low[curr] >= low[v])
+                        {
+                                low[curr] = low[v];
+                        }
+                        //add to articulation points list
+                        if ((parent[v] != -1)&&(low[v] >= depth[curr]))
+                        {
+                                articulationPoints.push_back(curr);
+                        }
+
+                }
+                else if (parent[v] != curr)
+                {
+                        if (depth[v] <= low[curr])
+                        {
+                                low[curr] = depth[v];
+                        }
+                }
+                //add to bridges
+                if (low[v] > depth[curr])
+                {
+                        std::pair<int, int> bride(curr, v);
+                        bridges.push_back(bride);
+                }
+        }
+        if ((parent[curr]== -1)&&children>1){
+             articulationPoints.push_back(curr);   
+        }
+}
+
+void findArticulationPointsAndBridges(Graph &G, std::vector<int> &articulationPoints, std::vector<std::pair<int, int>> &bridges)
+{
+        int n = G.n; // Number of Nodes
+        int curr = 0;
+        int d = 0;
+        std::vector<int> low(n, -1);
+        std::vector<int> depth(n, -1);
+        std::vector<int> parent(n, -1);
+        artdfs(G, curr, d, low, depth, parent, articulationPoints, bridges);
 }
