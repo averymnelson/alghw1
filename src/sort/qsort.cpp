099b288b8cd583f5042bfdb47749c84775b8a6f5
Avery Nelson <123122545+averymnelson@users.noreply.github.com>
2023-11-06 14:29:23 -0600

    uploadign the real quicksort

--- src/sort/qsort.cpp
+++ src/sort/qsort.cpp
@@ -3,58 +3,49 @@
 
 // extern
 std::string sortAlgName = "Quick Sort";
-
-template <class T>
-void heapify(std::vector<T> &arr, int N, int i)
+template<class T>
+int partition(std::vector<T> &arr, int start, int end)
 {
-
-    // Initialize largest as root
-    int largest = i;
-
-    // left = 2*i + 1
-    int l = 2 * i + 1;
-
-    // right = 2*i + 2
-    int r = 2 * i + 2;
-
-    // If left child is larger than root
-    if (l < N && arr[l] > arr[largest])
-        largest = l;
-
-    // If right child is larger than largest
-    // so far
-    if (r < N && arr[r] > arr[largest])
-        largest = r;
-
-    // If largest is not root
-    if (largest != i)
-    {
-        std::swap(arr[i], arr[largest]);
-
-        // Recursively heapify the affected
-        // sub-tree
-        heapify(arr, N, largest);
+    T pivot = arr[start];
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
     }
+ 
+    // Giving pivot element its correct position
+    int pivotIndex = start + count;
+    std::swap(arr[pivotIndex], arr[start]);
+ 
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (arr[i] <= pivot) {
+            i++;
+        }
+ 
+        while (arr[j] > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            std::swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotIndex;
 }
 
 template <class T>
 void sort(std::vector<T> &array, int l, int r)
 {
-    int N = array.size();
-    // Build heap (rearrange array)
-    for (int i = N / 2 - 1; i >= 0; i--)
-        heapify(array, N, i);
-
-    // One by one extract an element from heap
-    for (int i = N - 1; i > 0; i--)
-    {
-
-        // Move current root to end
-        std::swap(array[0], array[i]);
-
-        // call max heapify on the reduced heap
-        heapify(array, i, 0);
+    if (l >= r){
+        return;
     }
+    int p = partition(array, l, r); 
+    sort(array, l, p - 1);
+    sort(array, p + 1, r);
 }
 
 template void sort(std::vector<Edge> &array, int l, int r);
