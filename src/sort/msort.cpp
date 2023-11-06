f16cd2e88e7a0fc42c0e1db30ac57621a72ee32e
Avery Nelson <123122545+averymnelson@users.noreply.github.com>
2023-11-06 13:27:59 -0600

    uploading correct merge sort

--- src/sort/msort.cpp
+++ src/sort/msort.cpp
@@ -5,7 +5,73 @@
 std::string sortAlgName = "Merge Sort";
 
 template<class T>
+void merge(std::vector<T> &array, int const left, int const mid,
+           int const right)
+{
+    int const subArrayOne = mid - left + 1;
+    int const subArrayTwo = right - mid;
+ 
+    // Create temp arrays
+    auto *leftArray = new T[subArrayOne],
+        *rightArray = new T[subArrayTwo];
+ 
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (int i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (int j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+ 
+    auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+ 
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne
+           && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne]
+            <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray]
+                = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray]
+                = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+ 
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray]
+            = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+ 
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray]
+            = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+    delete[] leftArray;
+    delete[] rightArray;
+}
+ 
+
+template<class T>
 void sort(std::vector<T> &array, int l, int r) {
+    if (l >= r)
+        return;
+ 
+    int mid = l + (r - l) / 2;
+    sort(array, l, mid);
+    sort(array, mid + 1, r);
+    merge(array, l, mid, r);    
     // YOUR CODE HERE
     // Sorray array from l to r, i.e. sort array[l...r]
 }
