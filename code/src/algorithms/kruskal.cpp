ed0a7909f3e863f48d324f0610a19cce43d9d67e
crdildy <124619074+crdildy@users.noreply.github.com>
2023-11-27 09:53:43 -0600

    adding hw5 components and kruskal

---
+++ code/src/algorithms/kruskal.cpp
@@ -0,0 +1,31 @@
+#include <graph.hpp> 
+#include <sort.hpp> 
+#include <disjoint_set.hpp>
+
+//Perform unit test of the Kruskal's algorithm
+std::vector<Edge> constructMSTKruskal(Graph G) {
+    std::vector<Edge> edges = G.exportEdges(); // Graph's edges
+    //DisjointSet djs(G.n); 
+    // Use Disjoint Set to check wheter two vertices are on the same set
+    // Usage: Check djs.isOnSameSet(u, v); Check is u and v is on the same set or not
+    //              djs.join(u, v); Join sets of u and v into the same set
+    
+    // YOUR CODE HERE
+    //Sort the edges by weight
+    //For each edge e in sorted list do
+    std::sort(edges.begin(), edges.end());
+    
+    DisjointSet d(G.n); //cannot access number of vertices?
+    std::vector<Edge> mst;
+
+    for(const Edge& edge : edges){
+        int u = edge.u;
+        int v = edge.v;
+        if(!d.isOnSameSet(u, v)){
+            mst.push_back(edge);
+            d.join(u,v);
+        }
+    }
+    return mst;
+}
+
