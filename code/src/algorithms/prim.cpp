14bf5400dcbc314717c98ebf78169e16be594ece
Avery Nelson <123122545+averymnelson@users.noreply.github.com>
2023-11-27 11:22:31 -0600

    laptop dying

--- code/src/algorithms/prim.cpp
+++ code/src/algorithms/prim.cpp
@@ -14,13 +14,36 @@ struct EdgeKeyComparison {
 std::vector<Edge> constructMSTPrim(Graph G) {
     std::vector<Edge> edges = G.exportEdges(); // Graph's edges
 
-    // std::priority_queue< Edge, std::vector<Edge>, EdgeKeyComparison > heap;
+    std::priority_queue< Edge, std::vector<Edge>, EdgeKeyComparison > heap;
     // If you want to use heap to optimize the minimum searching, you can use heap defined as above
     //      Insert: heap.push(Edge(u, -1, distance));
     //      Get Minimum: top = heap.top(); u = top.u; distance = top.w;
     //      Remove top: heap.pop(); (this function usually goes after the get minimum method)
     
     // YOUR CODE HERE
-    
+    int src =0;
+    std::vector<int> key(G.getN(), INT_MAX);
+ 
+    // To store parent array which in turn store MST
+    std::vector<Edge> parent(G.getN(), -1);
+ 
+    // To keep track of vertices included in MST
+    std::vector<bool> inMST(G.getN(), false);
+ 
+    // Insert source itself in priority queue and initialize
+    // its key as 0.
+    heap.push(Edge(0, src, 0));
+    key[src] = 0;
+    while (!heap.empty()){
+        auto u = heap.top();
+        heap.pop();
+
+        if(inMST[u] == true){
+            continue;
+        }
+
+
+
+    }
 }
 
