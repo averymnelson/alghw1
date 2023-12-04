3340f3d4457ee9cfed0f8c127f5e0263226dbe51
averymnelson <averynelson2021@gmail.com>
2023-12-04 12:50:49 -0600

    i am stupid but have to do work for another class

--- src/algorithms/prim.cpp
+++ src/algorithms/prim.cpp
@@ -12,18 +12,63 @@ struct EdgeKeyComparison {
 
 
 std::vector<Edge> constructMSTPrim(Graph G) {
+    int u = 0; 
     std::vector<Edge> edges = G.exportEdges(); // Graph's edges
     std::vector<Edge> mst;
-    // std::priority_queue< Edge, std::vector<Edge>, EdgeKeyComparison > heap;
+    int size = G.getN();
+    std::priority_queue< Edge, std::vector<Edge>, EdgeKeyComparison > heap;
     // If you want to use heap to optimize the minimum searching, you can use heap defined as above
     //      Insert: heap.push(Edge(u, -1, distance));
     //      Get Minimum: top = heap.top(); u = top.u; distance = top.w;
     //      Remove top: heap.pop(); (this function usually goes after the get minimum method)
     
-    // YOUR CODE HERE
+    std::vector<bool> visit(N, false);
+    std::vector<int> dist(N, N);
+    std::vector<int> paths(N, 0);
+    std::vector<int> trace(N, -1);
+    
+    heap.push(start);
+    visit[start] = true;
+    paths[start]=1;
+    while(!heap.empty()){
     return mst;
 }
+top = heap.top(); 
+u = top.u; 
+distance = top.w;
+        int top = heap.pop();
+        if(top == destination){
+            sltn = true;
+            break;
+        }
+        
+        for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) 
+        {
+            u = top;
+            int v = p->value;
 
+            if(!visit[v]){
+                visit[v] = true;
+                heap.push(v);
+                trace[v] = top;
+                paths[v] = paths[top];
+                dist[v] = dist[top] + 1;
+            }
+            else if(dist[v] == (dist[u]+1)){
+                paths[v] = (paths[u]+paths[v]);
+            }
+        }
+        u = destination;
+         while (u != -1) {
+        mst.push_back(u);
+        u = trace[u];
+            }
+    std::reverse(mst.begin(),mst.end());
+
+    return mst; 
+    }
+
+    
 
 // int minKey(std::vector<int> key, std::vector<bool> inMST){
 //     int min=INT_MAX;
@@ -49,9 +94,7 @@ std::vector<Edge> constructMSTPrim(Graph G) {
     
 //     // YOUR CODE HERE
 //     //initialize
-//     std::vector<int> key(size, INT_MAX);
-//     std::vector<int> parent(size, -1);
-//     std::vector<bool> inMST(size, false);
+//     
 //     std::vector<Edge> results;
 //     std::priority_queue<Edge, std::vector<Edge>, EdgeKeyComparison > heap;
 //     //create first
